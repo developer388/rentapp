69b9927505647ab179b1d5255188bbaf1f149059
developer388 <nickhil388@yandex.com>
2017-06-08 23:37:14 +0530

    db error fixed

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ var env='local'
 var config ={
 	local :{
 		db :{
-		  connection_string : 'mongodb://localhost:27017/rentapp'
+		  connection_string : 'mongodb://rentapp:quickstart@ds115712.mlab.com:15712/heroku_rhhb4652'
 		},
 		app:{
 			port:8080,
