a04e1d0f9fc92ddb712642d460646a2833f1f978
developer388 <nickhil388@yandex.com>
2017-06-11 15:40:17 +0530

    clustered

---
+++ oserver.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var body_parser = require('body-parser');
+var mongoose = require('mongoose')
+var config = require('./app/config');
+var utils = require('./app/utils');
+var auth = require('./app/routes/auth')
+var admin = require('./app/routes/admin')
+var user = require('./app/routes/user');
+var app = express();
+
+mongoose.connect(config.db.connection_string);
+
+app.use(express.static(__dirname + '/public'));
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:true})); 
+
+app.use('/api/auth',auth);
+app.use('/api/admin', admin);
+app.use('/api/user', user);
+
+app.listen(config.app.port,function () {
+	console.log('Server started on port :'+config.app.port);
+});
--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 var express = require('express');
 var body_parser = require('body-parser');
+var cluster = require('cluster'); 
 var mongoose = require('mongoose')
 var config = require('./app/config');
 var utils = require('./app/utils');
 var auth = require('./app/routes/auth')
 var admin = require('./app/routes/admin')
 var user = require('./app/routes/user');
-var app = express();
-
 mongoose.connect(config.db.connection_string);
 
-app.use(express.static(__dirname + '/public'));
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended:true})); 
+if (cluster.isMaster) { 
+	var cpu_cores = require('os').cpus().length
+    var i=0;
+    for (i=0; i<cpu_cores; i++)
+    cluster.fork();
+ 	console.log('Master process with id '+process.pid+' has forked '+i+' workers.');   
+} else {
+	var app = express();
+	app.use(express.static(__dirname + '/public'));
+	app.use(body_parser.json());
+	app.use(body_parser.urlencoded({extended:true})); 
+	// app.use(function(req,res,next){
+	// 	console.log('request handled by '+process.pid);
+	// 	next();
+	// })
+	app.use('/api/auth',auth);
+	app.use('/api/admin', admin);
+	app.use('/api/user', user);
 
-app.use('/api/auth',auth);
-app.use('/api/admin', admin);
-app.use('/api/user', user);
+	app.listen(config.app.port,function () {
+		console.log('Worker '+process.pid+' listening on port :'+config.app.port);
+	});
+}
 
-app.listen(config.app.port,function () {
-	console.log('Server started on port :'+config.app.port);
-});
+
